50caa6739c69648977f5a8e875fb2c73159dd9bc
Alex Prokudin <prokudin89@gmail.com>
2019-01-03 19:25:16 +0700

    tuples

---
+++ Playgrounds/hello.playground/Pages/3. tuples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+let coords = (2, 3)
+
+let coordsDouble = (2.1, 3.5)
+
+let coordsMixed = (2.1, 3)
+
+let x1 = coords.0
+let y1 = coords.1
+
+let coordsNamed = (x: 2, y: 3)
+
+let x2 = coordsNamed.x
+let y2 = coordsNamed.y
+
+
+let coords3d = (x: 1, y: 2, z: 3)
+let (x3, y3, z3) = coords3d
+
+x3
+y3
+z3
+
+let (x4, y4, _) = coords3d
+
+x4
+y4
+
+//: [Next](@next)
