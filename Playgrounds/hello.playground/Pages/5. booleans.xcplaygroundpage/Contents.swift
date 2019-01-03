040f648d0088fc3293145893583dc9967c2d61e7
Alex Prokudin <prokudin89@gmail.com>
2019-01-03 19:57:49 +0700

    booleans

---
+++ Playgrounds/hello.playground/Pages/5. booleans.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+let yes: Bool = true
+let no: Bool = false
+
+let oneGreaterThanTwo = 1 > 2
+let oneLessThanTwo = 1 < 2
+
+6 <= 6
+
+6 == 7
+6 != 7
+
+6 == 6 && 5 != 4
+6 != 6 || 4 == 4
+!(6 == 6)
+
+let a = 5
+let b = 10
+var min: Int
+
+if a < b {
+    min = a
+} else {
+    min = b
+}
+
+// the same - ternary operator
+min = a < b ? a : b
+
+//: [Next](@next)
